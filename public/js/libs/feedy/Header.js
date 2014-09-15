f49527a4abbd0ddb89ec1439396a03da0a9ad132
Leeios <kellos@hotmail.fr>
2014-09-15 18:03:06 +0200

    debug syntax

--- public/js/libs/feedy/Header.js
+++ public/js/libs/feedy/Header.js
@@ -7,7 +7,7 @@ var Header = r.Seed.extend({
 
   tpl: function() {
     return {
-      tag: 'div.header',
+      tag: 'div.header.usual',
       children: [
         // { tag: 'img.banner', attr: { src: '/img/banner.jpg' } },
         { tag: 'div.new-project', innerHTML: 'New Project',
